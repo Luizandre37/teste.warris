f2e668075d3ac8318897dc49ede85ce8d70e789c
Luiz Andre Maceno <67300107+Luizandre37@users.noreply.github.com>
2023-05-30 17:08:50 -0300

    atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PagesComponent } from './components/pages/pages.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { ProdutoComponent } from './components/pages/produto/produto.component';
+import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
+import { HedearComponent } from './components/hedear/hedear.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HomeComponent,
+    LoginComponent,
+    ProdutoComponent,
+    CadastroComponent,
+    HedearComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

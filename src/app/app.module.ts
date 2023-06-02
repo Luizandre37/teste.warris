b4fbec656b616539c0fdce54508a5e2605b1d005
Luiz Andre Maceno <67300107+Luizandre37@users.noreply.github.com>
2023-06-02 12:12:16 -0300

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProdutoComponent } from './components/pages/produto/produto.component';
 import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
 import { HedearComponent } from './components/hedear/hedear.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { FooterComponent } from './components/footer/footer.component';
     ProdutoComponent,
     CadastroComponent,
     HedearComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
